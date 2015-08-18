d5cb380f5b56912c52ee5a8aade96ef78b78d7f8
Wayne Lai <wing50kimo@gmail.com>
2015-08-18 14:31:22 +0800

    Initial Commit

---
+++ MyCalendar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyCalendar
+//
+//  Created by B1403001 on 2015/8/18.
+//  Copyright (c) 2015年 B1403001. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyCalendar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyCalendar
+//
+//  Created by B1403001 on 2015/8/18.
+//  Copyright (c) 2015年 B1403001. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
